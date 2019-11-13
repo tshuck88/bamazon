260c088554c6fad684019a3fe091e83ab37681ac
Tyler Shuck <tshuck88@gmail.com>
2019-11-12 23:11:13 -0800

    added function to prompt user what product they want to purchase and to update the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,20 +12,64 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    displayItems();
+    initializeApp();
+
 });
 
-function displayItems() {
+function initializeApp() {
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         var table = new Table({
-            head: ['Product Id', 'Product Name', "Price ($)"]
-            , colWidths: [13, 20, 13]
+            head: ['Product Id', 'Product Name', "Price ($)"],
+            colWidths: [13, 20, 13]
         });
         for (let i = 0; i < results.length; i++) {
             let item = results[i];
             table.push([item.id, item.product_name, item.price.toFixed(2)])
         }
-        console.log(table.toString())
+        console.log(table.toString());
+        inquirer.prompt([
+            {
+                name: "product",
+                type: "rawlist",
+                choices: function () {
+                    let choiceArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].id);
+                    }
+                    return choiceArray;
+                },
+                message: "What product id would you like to buy?"
+            },
+            {
+                name: "amount",
+                type: "input",
+                message: "How many would you like to buy?"
+            }
+        ]).then(function (answer) {
+            let chosenItem; 
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].id === parseInt(answer.product)) {
+                    chosenItem = results[i];
+                }
+            }
+            if (parseInt(answer.amount) < chosenItem.stock_quantity) {
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [{
+                        stock_quantity: chosenItem.stock_quantity - answer.amount
+                    },
+                    {
+                        id: chosenItem.id
+                    }
+                    ], function (error) {
+                        if (error) throw err;
+                        console.log("Thank you for your purchase!");
+                        initializeApp();
+                    });
+            } else {
+                console.log("Sorry, there is not enough of that product in stock for your purchase. Please try again at a lower quantity.");
+                initializeApp();
+            }
+        })
     });
 }
