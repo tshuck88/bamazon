0df8e6715f0b64e51a6a6d52c7d98a283e1d7d60
Tyler Shuck <tshuck88@gmail.com>
2019-11-13 14:25:22 -0800

    created function to view products

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,7 +2,6 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const Table = require('cli-table');
 
-
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,6 +9,8 @@ const connection = mysql.createConnection({
     password: "tyler",
     database: "bamazonDB"
 });
+
+initializeManager();
 
 function initializeManager() {
     inquirer.prompt([
@@ -20,7 +21,7 @@ function initializeManager() {
             message: "What would you like to do?"
         }
     ]).then(function (answer) {
-        switch (answer) {
+        switch (answer.start) {
             case "View Products for Sale":
                 viewProducts();
                 break;
@@ -34,5 +35,21 @@ function initializeManager() {
                 addProduct();
                 break;
         }
+    });
+}
+
+function viewProducts() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        var table = new Table({
+            head: ['Product Id', 'Product Name', "Price ($)", "Stock Quantity"],
+            colWidths: [13, 20, 13, 20]
+        });
+        for (let i = 0; i < results.length; i++) {
+            let item = results[i];
+            table.push([item.id, item.product_name, item.price.toFixed(2), item.stock_quantity])
+        }
+        console.log(table.toString());
+        connection.end();
     });
 }
