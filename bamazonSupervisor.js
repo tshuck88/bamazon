c425bf39aabc361ea56b44e34b5213d3404971cb
Tyler Shuck <tshuck88@gmail.com>
2019-11-14 13:39:28 -0800

    created function to view department sales and profit

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -31,3 +31,19 @@ function initializeSupervisor() {
         }
     });
 }
+
+function viewDepartments() {
+    const table = new Table({
+        head: ['Department Id', 'Department Name', "Over Head Costs", "Product Sales ($)", "Total Profit ($)"],
+        colWidths: [20, 20, 20, 20, 20]
+    });
+    connection.query("SELECT *, SUM(product_sales) AS total_product_sales, SUM(product_sales)  - over_head_costs AS total_profit  FROM products INNER JOIN departments USING(department_name) GROUP BY department_name;",
+        function (err, results) {
+            if (err) throw err;
+            for (let i = 0; i < results.length; i++) {
+                let department = results[i];
+                table.push([department.department_id, department.department_name, department.over_head_costs, department.total_product_sales.toFixed(2), department.total_profit.toFixed(2)])
+            }
+            console.log(table.toString());
+        });
+}
